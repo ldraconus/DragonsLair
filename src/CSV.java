29212195a720571843cd707c4a4cccea47521a45
Mason Chamberlain <mchamberlain@unomaha.edu>
2019-10-17 13:54:34 -0500

    Updated printing information.

--- src/CSV.java
+++ src/CSV.java
@@ -8,104 +8,104 @@ import java.util.Scanner;
 //This class is responsible for opening a .csv file.
 public class CSV
 {
-	String name = null;
-	String fileLocation = null;
-	int length = 0;
-	String directory = null;
-	String extensionType = null;
-	int lastSlash;
-	int extension;
-	File csvFile;
-	BufferedReader reader = null;
+    String name = null;
+    String fileLocation = null;
+    int length = 0;
+    String directory = null;
+    String extensionType = null;
+    int lastSlash;
+    int extension;
+    File csvFile;
+    BufferedReader reader = null;
 
-	private static Scanner input = new Scanner(System.in);
+    private static Scanner input = new Scanner(System.in);
 
-	public void getInfo() {
-		System.out.println("\tFile name: " + name);
-		System.out.printf("\tFile length: %s lines\n", length);
-		System.out.println("\tFile extension: " + extensionType);
-	}
+    public void getInfo() {
+        System.out.println("\tFile name: " + name);
+        System.out.printf("\tFile length: %s lines\n", length);
+        System.out.println("\tFile extension: " + extensionType);
+    }
 
-	//Returns file path name. Use in conjunction with openFile after calling getFile.
-	public String getLocation() { return fileLocation; }
+    //Returns file path name. Use in conjunction with openFile after calling getFile.
+    public String getLocation() { return fileLocation; }
 
-	//getFile is preferred over setLocation. Only use if location is a known valid .csv file location.
-	public void setLocation(String location)
-	{
-		fileLocation = location;
-	}
+    //getFile is preferred over setLocation. Only use if location is a known valid .csv file location.
+    public void setLocation(String location)
+    {
+        fileLocation = location;
+    }
 
-	//Returns the length of the file that was read.
-	public int getLength() {return length;}
+    //Returns the length of the file that was read.
+    public int getLength() {return length;}
 
-	//Sets all necessary parameters of a file to be able to open it.
-	public void getFile() {
-		System.out.print("File to open: ");
-		fileLocation = input.nextLine();
-		lastSlash = fileLocation.lastIndexOf("/");
-		directory = fileLocation.substring(0, lastSlash);
-		extension = fileLocation.lastIndexOf(".");
-		extensionType = fileLocation.substring(extension + 1);
-		name = fileLocation.substring(lastSlash + 1, extension);
+    //Sets all necessary parameters of a file to be able to open it.
+    public void getFile() {
+        System.out.print("File to open: ");
+        fileLocation = input.nextLine();
+        lastSlash = fileLocation.lastIndexOf("/");
+        directory = fileLocation.substring(0, lastSlash);
+        extension = fileLocation.lastIndexOf(".");
+        extensionType = fileLocation.substring(extension + 1);
+        name = fileLocation.substring(lastSlash + 1, extension);
 
-		if (extensionType.compareTo("csv") != 0) {
-			System.out.print("Incorrect file type, please select a different file.\n");
-			fileReset();
-		}
-	}
+        if (extensionType.compareTo("csv") != 0) {
+            System.out.print("Incorrect file type, please select a different file.\n");
+            fileReset();
+        }
+    }
 
-	//Resets file information if the attempted file was not a .csv file.
-	private void fileReset() {
-		fileLocation = null;
-		lastSlash = 0;
-		directory = null;
-		extension = 0;
-		extensionType = null;
-		name = null;
-	}
+    //Resets file information if the attempted file was not a .csv file.
+    private void fileReset() {
+        fileLocation = null;
+        lastSlash = 0;
+        directory = null;
+        extension = 0;
+        extensionType = null;
+        name = null;
+    }
 
-	//Opens the provided file and prints all of the information.
-	public void openFile(String location) {
-		String line = "";
+    //Opens the provided file and prints all of the information.
+    public void openFile(String location) {
+        String line = "";
 
-		try {
-			reader = new BufferedReader(new FileReader(location));
+        try {
+            reader = new BufferedReader(new FileReader(location));
 
-			while ((line = reader.readLine()) != null) {
-				length++;
+            while ((line = reader.readLine()) != null) {
+                length++;
 
-				String[] fullLine = line.split(",");
+                String[] fullLine = line.split(",");
 
-				//for (int i = 0; i < fullLine.length; i++) {
-				//    System.out.printf("%-5s ", fullLine[i]);
-				//}
-				if (length > 4) {
-					System.out.printf("Diamond Number: %s\tName: %-60.60s\tQuantity: %2s", fullLine[1], fullLine[2], fullLine[0]);
-					System.out.println();
-				}
-				//System.out.println();
+                //for (int i = 0; i < fullLine.length; i++) {
+                //    System.out.printf("%-5s ", fullLine[i]);
+                //}
+                if (length > 4) {
+                    System.out.printf("Diamond Number: %s\tName: %-60.60s", fullLine[1], fullLine[2]);
+                    System.out.println();
+                }
+                //System.out.println();
 
-			}
-		}
+            }
+        }
 
-		catch (FileNotFoundException FNF) {
-			FNF.printStackTrace();
-		}
+        catch (FileNotFoundException FNF) {
+            FNF.printStackTrace();
+        }
 
-		catch (IOException IO) {
-			IO.printStackTrace();
-		}
+        catch (IOException IO) {
+            IO.printStackTrace();
+        }
 
-		finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				}
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                }
 
-				catch (IOException IO) {
-					IO.printStackTrace();
-				}
-			}
-		}
-	}
+                catch (IOException IO) {
+                    IO.printStackTrace();
+                }
+            }
+        }
+    }
 }
