0d9cf284998711dd2705d6a641d76de532b3ec3a
Kevin Rodenhausen <kevinrodenhausen@unomaha.edu>
2019-10-24 13:53:17 -0500

    Trying to fix my branch, and changed the insertItemTable method.

--- src/DB.java
+++ src/DB.java
@@ -1,10 +1,12 @@
 //import org.jetbrains.annotations.NotNull;
 
-import java.sql.*;
-import java.util.Arrays;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Vector;
 
-import com.sun.istack.internal.NotNull;
+//import com.sun.istack.internal.NotNull;
 
 public class DB {
 
@@ -14,8 +16,8 @@ public class DB {
         Connection(String host) {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","Aerospace1907");
-                //connection = DriverManager.getConnection(host + "?serverTimezone=US/Central", "root", "v4d3r@Laptop");
+                //connection = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","Aerospace1907");
+                connection = DriverManager.getConnection(host + "?serverTimezone=US/Central", "root", "password");
                 if (!DBExists("admin")) InitializeDB();
                 if (!TableExists("admin", "store")) InitializeStores();
             } catch (Exception e) { System.out.println(e); }
@@ -38,123 +40,98 @@ public class DB {
             ExecuteStatement("create database admin");
             ExecuteStatement("use admin");
             ExecuteStatement("create table user (id integer not null auto_increment, " +
-                                                     "name varchar(40) not null, " +
-                                                     "password char(40) not null, " +
-                                                     "store integer, " +
-                                                     "primary key(id))");
+                    "name varchar(40) not null, " +
+                    "password char(40) not null, " +
+                    "store integer, " +
+                    "primary key(id))");
             ExecuteStatement("insert into user (name, password) " +
-                                              "values ('admin', sha('admin'))");
+                    "values ('admin', sha('admin'))");
             InitializeStores();
         }
 
         void InitializeStores() {
             ExecuteStatement("use admin");
             ExecuteStatement("create table admin.store (id integer not null auto_increment, " +
-                                                            "name varchar(128), " +
-                                                            "primary key(id))");
+                    "name varchar(128), " +
+                    "primary key(id))");
         }
-        
+
         void createCustomerTable() {
-        	ExecuteStatement("create table customer(customer_id integer not null auto_increment, " + 
-        												"name varchar(40) not null, " +
-        												"phone varchar(20), " + 
-        												"email varchar(100), " + 
-        												"primary key(customer_id))");
+            ExecuteStatement("create table customer(id integer not null auto_increment, " +
+                    "name varchar(40) not null, " +
+                    "phone varchar(20), " +
+                    "email varchar(100), " +
+                    "primary key(id))");
         }
-        
+
         void createItemTable() {
-        	ExecuteStatement("create table item(item_id integer not null auto_increment, " +
-        											"item varchar(300) not null, " + 
-        											"diamond varchar(100), " +
-        											"primary key(item_id))");
+            ExecuteStatement("use admin");
+            ExecuteStatement("create table item(id integer not null auto_increment, " +
+                    "item varchar(300) not null, " +
+                    "diamond varchar(100), " +
+                    "primary key(id))");
         }
 
-        void insertItemTable(String itemName, String diamondCode,String store){
-            ExecutePrepared("use ?", store);
-            if(!itemExists(diamondCode)) {
-                db.ExecuteData("insert into item(item,diamond) values(?,?))", itemName, diamondCode);
-            }
-
-        }
-
-        Boolean itemExists(String diamondCode){
-            ResultSet theDiamond = db.ExecutePrepared("select diamond from item where diamond = ?",diamondCode);
-            try { return theDiamond != null && theDiamond.next(); }
-            catch (Exception e) { System.out.println(e); }
-            return false;
-
-        }
-        void deleteItemTable(String itemName){
-            db.ExecuteData("delete from item where item = ?",itemName);
-
-        }
-        
         void createPullListTable() {
-        	ExecuteStatement("create table pull_list(pull_id integer not null auto_increment, " +
-        											"customer_id integer, " +
-        											"item_id integer, " +
-        											"issue integer, " +
-        											"allVariants boolean, " +
-        											"quantity integer, " +
-        											"isGraphic boolean, " +
-        											"primary key(pull_id), " +
-        											"foreign key (customer_id) references customer(customer_id), "+
-        											"foreign key (item_id) references item(item_id))");
+            ExecuteStatement("create table pull_list(id integer not null auto_increment, " +
+                    "customer_id integer, " +
+                    "item_id integer, " +
+                    "issue integer, " +
+                    "allVariants boolean, " +
+                    "quantity integer, " +
+                    "isGraphic boolean, " +
+                    "primary key(id), " +
+                    "foreign key (customer_id) references customer(id), "+
+                    "foreign key (item_id) references item(id))");
         }
 
-        void createCustomerItemTable(){
-            ExecuteStatement("create table customerItem(customerItem_id integer not null auto_increment, " +
-                                                "wants integer, " +
-                                                "got integer, " +
-                                                "primary key(customerItem_id), " +
-                                                "foreign key(wants) references item(item_id), " +
-                                                "foreign key(got) references item(item_id))");
+        void createItemMapTable(){
+            ExecuteStatement("create table ItemMap(id integer not null auto_increment, " +
+                    "wants integer, " +
+                    "got integer, " +
+                    "primary key(id), " +
+                    "foreign key(wants) references item(id), " +
+                    "foreign key(got) references item(id))");
+        }
+
+        void createPullItemTable(){
+            ExecuteStatement("create table pullItem(id integer not null auto_increment, " +
+                    "item_id integer, " +
+                    "pull_id integer, " +
+                    "primary key(id), " +
+                    "foreign key(item_id) references item(id), " +
+                    "foreign key(pull_id) references pull_list(id))");
         }
 
         void createPullDateTable(){
-            ExecuteStatement("create table pullDate(pullDate_id integer not null auto_increment, " +
-                                                "pull_id integer, " +
-                                                "date date, " +
-                                                "primary key(pullDate_id), " +
-                                                "foreign key (pull_id) references pull_list(pull_id))");
-        }
-        void createPullItemTable(){
-            ExecuteStatement("create table pullItem(pullItem_id integer not null auto_increment, " +
-                                                "item_id integer, " +
-                                                "pull_id integer, " +
-                                                "primary key(pullItem_id), " +
-                                                "foreign key(item_id) references pull_list(item_id), " +
-                                                "foreign key(pull_id) references pull_list(pull_id))");
+            ExecuteStatement("create table pullDate(id integer not null auto_increment, " +
+                    "pull_id integer, " +
+                    "date date, " +
+                    "primary key(id), " +
+                    "foreign key (pull_id) references pull_list(id))");
         }
 
         void createQuantityTable(){
-            ExecuteStatement("create table quantity(quantity_id integer not null auto_increment, " +
-                                                "customer_id integer, " +
-                                                "pull_id integer, " +
-                                                "quantity integer, " +
-                                                "primary key(quantity_id), " +
-                                                "foreign key(customer_id) references pull_list(customer_id), " +
-                                                "foreign key(pull_id) references pull_list(pull_id))");
+            ExecuteStatement("create table quantity(id integer not null auto_increment, " +
+                    "customer_id integer, " +
+                    "pull_id integer, " +
+                    "quantity integer, " +
+                    "primary key(id), " +
+                    "foreign key(customer_id) references customer(id), " +
+                    "foreign key(pull_id) references pull_list(id))");
         }
 
 
 
         void InitializeStore(String store) {
-            ExecutePrepared("create database ?", store);
-            ExecutePrepared("use ?", store);
-            // create customer table
+            ExecuteStatement("create database " + store);
+            ExecuteStatement("use " + store);
             createCustomerTable();
-            // create item table
             createItemTable();
-            // create pull_list table
             createPullListTable();
-            //create customerItem table
-            createCustomerItemTable();
-            //create pullDate table
+            createItemMapTable();
             createPullDateTable();
-            //create pullItem table
             createPullItemTable();
-            //create quantity table
             createQuantityTable();
         }
 
@@ -199,8 +176,8 @@ public class DB {
 
         private boolean TableExists(String db, String name) {
             String sql = "SELECT * " +
-                         "FROM information_schema.TABLES " +
-                         "WHERE (TABLE_SCHEMA = ?) AND (TABLE_NAME = ?)";
+                    "FROM information_schema.TABLES " +
+                    "WHERE (TABLE_SCHEMA = ?) AND (TABLE_NAME = ?)";
             ResultSet r = ExecutePrepared(sql, db, name);
             try { return r != null && r.next(); }
             catch (Exception e) { System.out.println(e); }
@@ -228,7 +205,7 @@ public class DB {
         Init(defaultHost);
     }
 
-    public boolean Login(String username, @NotNull char[] password) {
+    public boolean Login(String username, char[] password) {
         String pass = "";
         for (Character c: password) pass += c;
         return db.Login(username, pass);
@@ -252,8 +229,8 @@ public class DB {
 
     public boolean StoreInUse(String store) {
         ResultSet data = db.ExecutePrepared("select admin.store.id " +
-                                                 "from admin.store, admin.user " +
-                                                 "where admin.store.name = ? and admin.user.store = admin.store.id", store);
+                "from admin.store, admin.user " +
+                "where admin.store.name = ? and admin.user.store = admin.store.id", store);
         try { return data != null && data.next(); }
         catch (Exception e) { System.out.println(e); }
         return false;
@@ -261,8 +238,8 @@ public class DB {
 
     public boolean StoreExists(String store) {
         ResultSet data = db.ExecutePrepared("select id " +
-                                                 "from admin.store " +
-                                                 "where name = ?", store);
+                "from admin.store " +
+                "where name = ?", store);
         try { return data != null && data.next(); }
         catch (Exception e) { System.out.println(e); }
         return false;
@@ -270,6 +247,7 @@ public class DB {
 
     public void AddStore(String store) {
         db.ExecuteData("insert into admin.store (name) values(?)", store);
+        db.InitializeStore(store);
     }
 
     public void DeleteStore(String store) {
@@ -321,7 +299,7 @@ public class DB {
     public void AddUser(String user, String password, String store) {
         int id = GetStoreId(store);
         db.ExecuteData("insert into admin.user (name, password, store) values(?, sha(?), ?)",
-                       user, password, Integer.toString(id));
+                user, password, Integer.toString(id));
     }
 
     public void DeleteUser(String user) {
@@ -333,13 +311,13 @@ public class DB {
         if (password.isEmpty()) db.ExecuteData("update admin.user set admin.user.name = ?, admin.user.store = ? " +
                 "where admin.user.name = ?", name, Integer.toString(id), orig);
         else db.ExecuteData("update admin.user set admin.user.name = ?, admin.user.password = sha(?), admin.user.store = ? " +
-                                              "where admin.user.name = ?", name, password, Integer.toString(id), orig);
+                "where admin.user.name = ?", name, password, Integer.toString(id), orig);
     }
 
     public String GetUserStore(String user) {
         ResultSet r = db.ExecutePrepared("select store.name from admin.user, admin.store " +
-                                                                "where admin.user.name = ? " +
-                                                                  "and admin.user.store = admin.store.id", user);
+                "where admin.user.name = ? " +
+                "and admin.user.store = admin.store.id", user);
         if (r == null) return "";
         try {
             if (!r.next()) return "";
@@ -351,7 +329,8 @@ public class DB {
 
     public Vector<String> GetCustomers() {
         Vector<String> customers = new Vector<>();
-        ResultSet data = db.ExecutePrepared("select name from ?.customers", Data.Store());
+        db.ExecuteStatement("use " + Data.Store());
+        ResultSet data = db.ExecutePrepared("select name from customers");
 
         try {
             if (data != null) {
@@ -363,5 +342,79 @@ public class DB {
         catch(Exception e) { System.out.println(e); }
 
         return customers;
+    }
+
+    public boolean CustomerExists(String store, String customer) {
+        db.ExecuteStatement("use " + store);
+        ResultSet data = db.ExecutePrepared("select id " +
+                "from customer " +
+                "where name = ?", customer);
+        try { return data != null && data.next(); }
+        catch (Exception e) { System.out.println(e); }
+        return false;
+    }
+
+    public void AddCustomer(String store, String name, String email, String phone) {
+        db.ExecuteStatement("use " + store);
+        db.ExecuteData("insert into customer (name, email, phone) values(?, ?, ?)",
+                name, email, phone);
+    }
+
+    public String GetCustomerEMail(String store, String name) {
+        db.ExecuteStatement("use " + store);
+        ResultSet r = db.ExecutePrepared("select customer.email from customer " +
+                "where customer.name = ? ", name);
+        if (r == null) return "";
+        try {
+            if (!r.next()) return "";
+            return r.getString("customer.email");
+        }
+        catch (Exception e) { System.out.println(e); }
+        return "";
+    }
+
+    public String GetCustomerPhone(String store, String name) {
+        db.ExecuteStatement("use " + store);
+        ResultSet r = db.ExecutePrepared("select customer.phone from ?.customer " +
+                "where customer.name = ? ", name);
+        if (r == null) return "";
+        try {
+            if (!r.next()) return "";
+            return r.getString("customer.phone");
+        }
+        catch (Exception e) { System.out.println(e); }
+        return "";
+    }
+
+    public void UpdateCustomer(String store, String origCustomer, String customer, String email, String phone) {
+        db.ExecuteStatement("use " + store);
+        db.ExecuteData("update customer set name=?, email=?, phone=? where name=?",
+                customer, email, phone, origCustomer);
+    }
+
+    public void DeleteCustomer(String store, String origCustomer) {
+        db.ExecuteStatement("use " + store);
+        db.ExecuteData("delete from customer where name=?",
+                origCustomer);
+    }
+
+
+    void insertItemTable(String itemName, String diamondCode){
+        if(!itemExists(diamondCode)) {
+            db.ExecuteData("insert into item(item,diamond) values(?,?))", itemName, diamondCode);
+        }
+
+    }
+
+    Boolean itemExists(String diamondCode){
+        ResultSet theDiamond = db.ExecutePrepared("select diamond from item where diamond = ?",diamondCode);
+        try { return theDiamond != null && theDiamond.next(); }
+        catch (Exception e) { System.out.println(e); }
+        return false;
+
+    }
+    void deleteItemTable(String itemName){
+        db.ExecuteData("delete from item where item = ?",itemName);
+
     }
 }
