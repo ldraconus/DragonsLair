9e3301d1efe4e3d0b1e8f4e07853349f0f98f471
brivera1 <47227827+brivera1@users.noreply.github.com>
2019-11-05 16:52:29 -0600

    Add comments to classes.

--- src/MainForm.java
+++ src/MainForm.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.util.Vector;
 
+/**
+ * Main form of the application.
+ */
 public class MainForm {
     private JPanel MainPanel;
     private JButton manageCustomersButton;
@@ -15,6 +18,9 @@ public class MainForm {
     private static MainForm mainForm = null;
     private static JFrame frame = null;
 
+    /**
+     * Create a new JFrame and display the initial UI components.
+     */
     public static void Display() {
         frame = new JFrame("Dragon's Lair Pull-list Actions");
         frame.setContentPane(new MainForm().MainPanel);
@@ -24,7 +30,11 @@ public class MainForm {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-    
+
+    /**
+     * Constructor of this class.
+     * Setup action listeners to UI components.
+     */
     public MainForm() {
         mainForm = this;
         MainPanel.registerKeyboardAction(new ActionListener() {
@@ -82,6 +92,9 @@ public class MainForm {
         });
     }
 
+    /**
+     * Enable or disable form buttons based on if the data store is empty or not.
+     */
     private void SetContext() {
         boolean flag = !Data.Store().isEmpty();
         manageCustomersButton.setEnabled(flag);
@@ -98,26 +111,47 @@ public class MainForm {
         if (!Data.Store().isEmpty()) storeComboBox.setSelectedItem(Data.Store());
     }
 
+    /**
+     * Set the Data Store.
+     */
     private void StorePicked() {
         Data.Store((String) storeComboBox.getSelectedItem());
     }
 
+    /**
+     * Display the Preferences Form.
+     */
     private void EditPreferences() {
         PrefsForm.Display();
     }
 
+    /**
+     * FUTURE: Display the Print Reports Form.
+     */
     private void PrintReports() {
     }
 
+    /**
+     * FUTURE: Display the CSV Form.
+     */
     private void ImportCSV() {CSVForm.Display();  }
 
+    /**
+     * FUTURE: Display the Manage Comics Form.
+     */
     private void ManageItems() {
     }
 
+    /**
+     * Display the Manage Customers Form.
+     */
     private void ManageCustomers() {
         ManageCustomersForm.Display();
     }
 
+    /**
+     * Dispose of all forms and then display the Login Form.
+     */
     private void LogOut(){
         frame.dispose();
         LoginForm.Dispose();
--- src/PrefsForm.java
+++ src/PrefsForm.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.event.*;
 
+/**
+ * Provides form and functionality to manage system preferences.
+ */
 public class PrefsForm {
     private JButton manageStoresButton;
     private JPanel PrefsPanel;
@@ -10,13 +13,24 @@ public class PrefsForm {
     private static JFrame frame;
     public static PrefsForm prefsForm = null;
 
+    /**
+     * Returns the JFrame.
+     * @return JFrame.
+     */
     public static JFrame Frame() { return frame; }
+
+    /**
+     * Dispose the JFrame.
+     */
     public static void Dispose() { // dispose sub window
         ManageUsersForm.Dispose();
         ManageStoresForm.Dispose();
         if (frame != null) frame.dispose();
     }
 
+    /**
+     * Create a new JFrame and display the initial UI components.
+     */
     public static void Display() {
         if (frame == null) {
             frame = new JFrame("Dragon's Lair Preferences");
@@ -28,6 +42,10 @@ public class PrefsForm {
         frame.setVisible(true);
     }
 
+    /**
+     * Class constructor.
+     * Setup action listeners for UI components.
+     */
     public PrefsForm() {
         prefsForm = this;
         PrefsPanel.registerKeyboardAction(new ActionListener() {
@@ -59,24 +77,42 @@ public class PrefsForm {
         });
     }
 
+    /**
+     * Set context.
+     */
     private void Open() {
         SetContext();
     }
 
+    /**
+     * Set the Preference Form context.
+     */
     public static void setContext() {
         prefsForm.SetContext();
     }
 
+    /**
+     * Enable or disable manage login button.
+     */
     private void SetContext() {
         manageLoginsButton.setEnabled(Data.DB().GetStores().size() != 0);
     }
 
+    /**
+     * Display Manage Users Form.
+     */
     private void ManageLogins() {
         ManageUsersForm.Display();
     }
 
+    /**
+     * Display Manage Stores Form.
+     */
     private void ManageStores() { ManageStoresForm.Display(); }
 
+    /**
+     * Hide JFrame.
+     */
     private void Done() {
         frame.setVisible(false);
     }
